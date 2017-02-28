376f6d9c31059b5781a60545fca23224ee3bfae2
zhangkaili <172912992@qq.com>
2017-02-28 13:09:57 +0800

    修复部分bug

--- update/11-29/CSVA/public/lib/leftsidebar.js
+++
@@ -1,9 +0,0 @@
-export default function slidebar() {
-   if($('.link').length>1){
-       $('.link').click(function(){
-           $(this).parent('li').toggleClass('open');
-           $(this).next().slideToggle();
-           $(this).parent('li').siblings().removeClass('open').children('ul').slideUp()
-       })
-   }
-}
